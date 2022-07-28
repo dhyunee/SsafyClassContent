2b6ebde0eb630375a4370d2980ededa7e4e71fe8
songb <rlthdqls@gmail.com>
2022-07-28 16:23:21 +0900

    json

--- java_ssafy_8th_l/src/com/ssafy/live5/gui/LayoutTest.java
+++ java_ssafy_8th_l/src/com/ssafy/live5/gui/LayoutTest.java
@@ -1,6 +1,7 @@
 package com.ssafy.live5.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Panel;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
@@ -9,8 +10,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.plaf.IconUIResource;
 
-public class LayoutTest extends JFrame {
+import com.ssafy.live4.collection.set.SetTest;
+
+public class LayoutTest extends JFrame {//5개 영역으로 나뉨
     private static final long serialVersionUID = 1L;
     private JLabel lNum1, lNum2, lCorrectCnt, lWrongCnt;
     private JTextField tfAns;
@@ -28,9 +32,9 @@ public class LayoutTest extends JFrame {
         makeProblemPanel();
         // Frame 기본 설정
         this.setTitle("layout test");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//버튼 닫으면 죽음
         this.pack();
-        this.setVisible(true);
+        this.setVisible(true);//화면에 보여줘라
     }
     
     private void makeProblem() {
@@ -45,6 +49,16 @@ public class LayoutTest extends JFrame {
     
     private void makeResultPanel() {
         // TODO: 결과를 보여줄 panel을 구성하시오.
+    	JPanel panel =new JPanel();
+    	
+    	this.add(new JLabel("정답갯수: "));
+    	lCorrectCnt=new JLabel("0");
+    	panel.add(lCorrectCnt);
+    	panel.add(new JLabel("오답갯수: "));
+    	lWrongCnt=new JLabel("0");
+    	panel.add(lWrongCnt);
+    	this.add(panel,BorderLayout.SOUTH);
+    	
         // END:
     }
 
@@ -52,6 +66,35 @@ public class LayoutTest extends JFrame {
     
     private void makeProblemPanel() {
         // TODO: 문제를 출제할 패널을 구성하시오.
+    	JPanel panel=new JPanel();
+    	lNum1=new JLabel();
+    	lNum2=new JLabel();
+    	panel.add(lNum1);
+    	panel.add(new JLabel("*"));
+    	panel.add(lNum2);
+    	panel.add(new JLabel("="));
+    	tfAns=new JTextField(10);
+    	panel.add(tfAns);
+    	makeProblem();
+    	tfAns.addKeyListener(new KeyAdapter() {
+    		@Override
+    		public void keyReleased(KeyEvent e) {
+    			if(e.getKeyCode()==10) {
+    				int val=Integer.parseInt(tfAns.getText());
+    				if(val==answer) {
+    					int pre=Integer.parseInt(lCorrectCnt.getText());
+    					lCorrectCnt.setText(pre+1+"");
+    				}else {
+        				int pre=Integer.parseInt(lWrongCnt.getText());
+        				lWrongCnt.setText(pre+1+"");
+        				
+    				
+    				}
+    				makeProblem();
+    			}
+    		}
+		});
+    	this.add(panel,BorderLayout.CENTER);
         // END:
     }
 
