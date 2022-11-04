5d30c896fed817506f2ea59c04227f8f7221d5d1
dhyunee <101089655+dhyunee@users.noreply.github.com>
2022-11-04 17:51:56 +0900

    1104
    Former-commit-id: bef85d6ad29b7dd393e1639d7c0607e27bd8e96f

--- SPRING/SpringBootMore-1/src/main/java/com/mycom/more/controller/StudentController.java
+++ SPRING/SpringBootMore-1/src/main/java/com/mycom/more/controller/StudentController.java
@@ -3,15 +3,28 @@ package com.mycom.more.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycom.more.dto.StudentDto;
 import com.mycom.more.service.StudentService;
+
+@CrossOrigin(
+		origins="http://127.0.0.1:5500",
+		allowCredentials="true",//session id를 기존처럼 고정=>server session 계속 사용할 수 있도록
+		allowedHeaders="*",
+		methods= {
+				RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE,
+				RequestMethod.OPTIONS,RequestMethod.HEAD
+		}
+)
+
 
 @RestController
 public class StudentController {
