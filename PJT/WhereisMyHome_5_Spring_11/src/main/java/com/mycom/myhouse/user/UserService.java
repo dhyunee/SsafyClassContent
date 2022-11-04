a40460750bd00976a5b907b10e191550eb1f25a7
dhyunee <101089655+dhyunee@users.noreply.github.com>
2022-11-04 17:56:33 +0900

    1102pjt
    Former-commit-id: 44af8d525a7843064a745b14530d750bba64f1d2

---
+++ PJT/WhereisMyHome_5_Spring_11/src/main/java/com/mycom/myhouse/user/UserService.java
@@ -0,0 +1,12 @@
+package com.mycom.myhouse.user;
+
+import com.mycom.myhouse.user.dto.UserDto;
+import com.mycom.myhouse.user.dto.UserResultDto;
+
+public interface UserService {
+	UserResultDto insertUser(UserDto userDto);
+	UserResultDto updateUser(UserDto userDto);
+	UserResultDto deleteUser(String userEmail);
+	UserDto loginUser(UserDto dto);
+	UserDto getUser(String userEmail);
+}
