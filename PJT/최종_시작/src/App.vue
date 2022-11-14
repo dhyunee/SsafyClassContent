c332a9edf441a60ca07798bd40703f775240f9c0
dhyunee <101089655+dhyunee@users.noreply.github.com>
2022-11-14 17:11:19 +0900

    finalpjt_start
    Former-commit-id: 74c38b0336c09e5969b86d1b8e80a5ad843a6d56

---
+++ PJT/최종_시작/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav-page></nav-page>
+    <main-page></main-page>
+    <footer-page></footer-page>
+  </div>
+</template>
+
+<script>
+import FooterPage from "@/components/FooterPage.vue";
+//import MainPage from "@/components/MainPage.vue";
+import NavBar from "@/components/NavBar.vue";
+import MapPage from "@/components/MapPage.vue";
+//import MapPage from "@/components/MapPageFull.vue";
+export default {
+  components: {
+    "footer-page": FooterPage,
+    "main-page": MapPage,
+    "nav-page": NavBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
